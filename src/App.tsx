d0425d8c03ad3c88fd080c7a82e899a8ea4c0797
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-27 01:17:05 +0300

    replace home with products

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
