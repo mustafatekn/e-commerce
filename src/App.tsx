51487e757e1e058fc4d97f46fe60c68796a4a59c
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-27 01:34:58 +0300

    create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Products from "./pages/Products";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Products />} />
       </Routes>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-primary-blue py-1">
+        <div className="container mx-auto text-white flex items-center justify-between">
+            <div className="text-3xl">Eteration</div>
+        </div>
+    </nav>
+  )
+}
