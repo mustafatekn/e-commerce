cfe8eadc858f6c2049c9adfaa2573019069db5f1
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-28 01:55:51 +0300

    fix listing searched brands and models

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -1,7 +1,8 @@
 import { useAppDispatch, useAppSelector } from "../store"
 import { addSelectedBrand, filterProducts, removeSelectedBrand, setSearchedBrands } from "../store/features/productSlice";
-
+import { useState } from 'react'
 export default function Brands() {
+  const [searchText, setSearchText] = useState("");
   const brands = useAppSelector(state => state.products.brands);
   const searchedBrands = useAppSelector(state => state.products.searchedBrands);
 
@@ -12,13 +13,16 @@ export default function Brands() {
       <small className="text-slate-500">Brands</small>
       <div className="shadow-md bg-white pl-3 py-2 my-2">
         <div className="relative flex items-center flex-1 pr-3">
-          <input type="text" className="bg-slate-50 inline-block w-full px-7 py-1 rounded-sm my-1" placeholder="Search" onChange={(e) => dispatch(setSearchedBrands(e.target.value))} />
+          <input type="text" className="bg-slate-50 inline-block w-full px-7 py-1 rounded-sm my-1" placeholder="Search" onChange={(e) => {
+            setSearchText(e.target.value)
+            dispatch(setSearchedBrands(e.target.value))
+          }} />
           <svg role="img" className="w-4 h-4 inline-block absolute left-2">
             <use xlinkHref="/assets/sprite.svg#search" />
           </svg>
         </div>
         <div className="overflow-y-scroll custom-scrollbar h-48">
-          {searchedBrands.length > 0 ? searchedBrands.map((brand: String, index: number) => (
+          {searchText ? searchedBrands.map((brand: String, index: number) => (
             <div key={index}><input type="checkbox" onChange={(e) => {
               if (e.target.checked) {
                 dispatch(addSelectedBrand(brand))
