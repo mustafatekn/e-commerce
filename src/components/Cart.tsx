49c922cff2e05e294de9088937b249507c874c3e
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-28 01:48:44 +0300

    check if cart contains any item

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -18,7 +18,7 @@ export default function Cart() {
     <div className="my-2 w-64">
       <small className="text-slate-500">Cart</small>
       <div className="shadow-md bg-white px-5 py-2 my-2 space-y-2">
-        {cart.length > 0 && cart.map((item: CartItem, index: number) => (
+        {cart.length > 0 ? cart.map((item: CartItem, index: number) => (
           <div className="flex justify-between items-center" key={index}>
             <div className="flex flex-col">
               <span>{item.product.name}</span>
@@ -30,7 +30,11 @@ export default function Cart() {
               <button className="h-8 w-7 bg-slate-200 rounded-sm" type="button" onClick={() => handleIncrement(item)}>+</button>
             </div>
           </div>
-        ))}
+        )) : (
+          <div className="h-20">
+            <span className="text-sm text-gray-500">Your cart is empty.</span>
+          </div>
+        )}
       </div>
     </div>
   )
