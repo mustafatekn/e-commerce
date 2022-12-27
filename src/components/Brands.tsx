342e27b871e22a549d597e49b4039f2e130bfac4
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-27 18:15:31 +0300

    create custom scrollbar

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -6,16 +6,19 @@ export default function Brands() {
   return (
     <div className="my-2 w-64">
       <small className="text-slate-500">Brands</small>
-      <div className="shadow-md bg-white px-5 py-2 my-2 h-48 overflow-y-scroll">
-        <div className="relative flex items-center flex-1">
+      <div className="shadow-md bg-white pl-3 py-2 my-2">
+        <div className="relative flex items-center flex-1 pr-3">
           <input type="text" className="bg-slate-50 inline-block w-full px-7 py-1 rounded-sm my-1" placeholder="Search" />
           <svg role="img" className="w-4 h-4 inline-block absolute left-2">
             <use xlinkHref="/assets/sprite.svg#search" />
           </svg>
         </div>
-        {brands.length > 0 && brands.map((brand: String, index: number) => (
-          <div key={index}><input type="checkbox" /><label className="ml-2">{brand}</label></div>
-        ))}
+        <div className="overflow-y-scroll custom-scrollbar h-48">
+          {brands.length > 0 && brands.map((brand: String, index: number) => (
+            <div key={index}><input type="checkbox" /><label className="ml-2">{brand}</label></div>
+          ))}
+        </div>
+
       </div>
     </div>
   )
--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -7,16 +7,19 @@ export default function Model() {
   return (
     <div className="my-2 w-64">
       <small className="text-slate-500">Model</small>
-      <div className="shadow-md bg-white px-5 py-2 my-2 h-48 overflow-y-scroll">
-        <div className="relative flex items-center flex-1">
+      <div className="shadow-md bg-white py-2 my-2 pl-3">
+        <div className="relative flex items-center flex-1 pr-3">
           <input type="text" className="bg-slate-50 inline-block w-full px-7 py-1 rounded-sm my-1" placeholder="Search" />
           <svg role="img" className="w-4 h-4 inline-block absolute left-2">
             <use xlinkHref="/assets/sprite.svg#search" />
           </svg>
         </div>
-        {models.length > 0 && models.map((model: String, index: number) => (
-          <div key={index}><input type="checkbox" /><label className="ml-2">{model}</label></div>
-        ))}
+        <div className="overflow-y-scroll custom-scrollbar h-48">
+          {models.length > 0 && models.map((model: String, index: number) => (
+            <div key={index}><input type="checkbox" /><label className="ml-2">{model}</label></div>
+          ))}
+        </div>
+
       </div>
     </div>
   )
