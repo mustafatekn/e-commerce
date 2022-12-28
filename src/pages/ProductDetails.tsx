7212a59dd996f82e5bd925ecfeac7fb9b954d4cf
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-28 21:51:46 +0300

    improve responsive ui on product details

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -36,7 +36,7 @@ export default function ProductDetails() {
             <MobileFilter />
             <MobileSort />
             {product &&
-                <div className="bg-white shadow-md mr-6">
+                <div className="bg-white shadow-md mr-6 hidden lg:block">
                     <div className="flex space-x-5 p-3">
                         <div className="w-1/2"><img src={product.image} alt={product.name} className="object-cover object-center block w-full" /> </div>
                         <div className="w-1/2 px-5">
@@ -47,6 +47,15 @@ export default function ProductDetails() {
                         </div>
                     </div>
                 </div>}
+            {product && <div className="bg-white shadow-md mx-5 lg:hidden block">
+                <div className="p-4">
+                    <img src={product.image} alt={product.name} className="object-cover object-center block w-full" />
+                    <h1 className="text-base sm:text-xl mt-2">{product.name}</h1>
+                    <h2 className="text-sm sm:text-lg text-primary-blue mt-1">{product.price} ₺</h2>
+                    <button type="button" className="bg-primary-blue block w-full px-1 py-2 rounded-md text-white font-bold mt-5" onClick={() => handleClick(product)} >Add to Cart</button>
+                    <p className="mt-5 text-sm sm:text-base break-before-all">{product.description}</p>
+                </div>
+            </div>}
         </DefaultLayout>
     )
 }
