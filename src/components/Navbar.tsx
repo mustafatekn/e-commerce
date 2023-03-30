d49eb0723d2d05480d534ffcf897668f16d5fbf0
Mustafa Osman Tekin <mustafateknn@gmail.com>
2023-03-30 16:24:06 +0300

    implement netmera

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
         <>
             <nav className="bg-primary-blue py-1 hidden lg:block">
                 <div className="container md:mx-auto text-white flex items-center">
-                    <Link to="/" className="text-lg md:text-xl xl:text-3xl w-auto md:w-28 lg:w-56 xl:w-64 ml-6 lg:ml-0">Eteration</Link>
+                    <Link to="/" className="text-lg md:text-xl xl:text-3xl w-auto md:w-28 lg:w-56 xl:w-64 ml-6 lg:ml-0">E-commerce</Link>
                     <Search />
                     <div className="lg:w-56 xl:w-64 flex items-center">
                         <div>{totalPrice} ₺</div>
@@ -26,7 +26,7 @@ export default function Navbar() {
 
             <nav className="bg-primary-blue py-1 block lg:hidden px-5">
                 <div className="container mx-auto text-white flex items-center justify-between lg:justify-around">
-                    <Link to="/" className="text-lg md:text-xl xl:text-3xl">Eteration</Link>
+                    <Link to="/" className="text-lg md:text-xl xl:text-3xl">E-commerce</Link>
                     <div className="flex items-center">
                         <button type="button" className="flex items-center" onClick={() => dispatch(toggleSort())}><svg role="img" className="w-5 h-5 inline-block text-white ml-3" fill="white">
                             <use xlinkHref="/assets/sprite.svg#sort" />
